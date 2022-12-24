d5601b791b4cd6dd1c17c4bffb8c226d33fc2cea
AntonAcorn <ShustikovAnimation@gmail.com>
2022-12-24 14:40:32 +0300

    Responding with xml or Json

--- src/main/java/com/acorn/mobileappws/controller/UserController.java
+++ src/main/java/com/acorn/mobileappws/controller/UserController.java
@@ -6,6 +6,7 @@ import com.acorn.mobileappws.service.UserService;
 import com.acorn.mobileappws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,7 +19,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(path = "/{id}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public UserRest getUser(@PathVariable String id){
         UserRest userToReturn = new UserRest();
         UserDto userDto = userService.getUserByUserId(id);
